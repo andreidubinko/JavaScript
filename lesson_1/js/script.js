028b092225cf2a84991722aca5e5e28d07e29780
Andrei <andrei.dubinko@gmail.com>
2018-03-26 19:38:09 +0300

    added HomeWork_1

---
+++ lesson_1/js/script.js
@@ -0,0 +1,21 @@
+var budget = prompt("Ваш бюджет?");
+var shopName = prompt("Название вашего магазина?")
+
+var shopGoods = [];  // объявляем наш массив
+
+for (var i = 0; i < 3; i++) {    // задаём вопрос три раза
+  shopGoods.push(prompt('Какой тип товаров будем продовать? ' + (i+1))); // пушим значение в массив
+}
+
+mainList = {
+	budget, 
+	shopName,
+	shopGoods,
+	employers:{},
+	open: true
+}
+
+console.log(mainList);
+
+var budgetPerDay = budget / 30;
+alert("Ваш бюджет на один день из месяца равен " + budgetPerDay);
