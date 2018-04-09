6cb6c316dd178bab43981beed27eb79caa79822e
Andrei <andrei.dubinko@gmail.com>
2018-04-09 22:13:16 +0300

    lesson 9 + lesson 9 input

--- lesson_9/js/script.js
+++ lesson_9/js/script.js
@@ -68,10 +68,6 @@ window.addEventListener('DOMContentLoaded', function(){
 				hours.innerHTML = t.hours;
 				minutes.innerHTML = t.minutes;
 				seconds.innerHTML = t.seconds;
-
-				if (t.total <= 0) {
-					clearInterval(timeInterval);
-				}
 			};
 			updateClock();
 			let timeInterval = setInterval(updateClock, 1000);
@@ -84,9 +80,19 @@ window.addEventListener('DOMContentLoaded', function(){
 	let more = document.querySelector('.more'),
 		overlay = document.querySelector('.overlay'),
 		close = document.querySelector('.popup-close');
-
+		// при нажатии на конпку появляется модальное окно
 	more.addEventListener('click', function() {
 		this.classList.add('more-splash');
-		overlay.style.display = "block";
+		overlay.style.display = "block"; // показать наше модальное окно при клике на кнопку
+		// чтобы модальное окно не прокручивалось 
+		document.body.style.overflow = 'hidden';
 	});
+		// при нажатии на крестик закрываем
+	close.addEventListener('click', function(){
+		overlay.style.display = "none";
+		more.classList.remove('more-splash');
+		// возвращаем возможность прокрутки после закрытия модального окна
+		document.body.style.overflow = '';
+	})	
+
 });
